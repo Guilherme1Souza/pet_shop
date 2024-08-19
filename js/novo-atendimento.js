2c36468c42b4541de6d9863c7370dd90ff25f2fc
Choconaldo <audioafrfb@gmail.com>
2024-08-19 15:04:35 -0300

    finalizado JS de editar atendimento

--- js/novo-atendimento.js
+++ js/novo-atendimento.js
@@ -12,18 +12,44 @@ const btnEl = document.getElementById("btn");
 
 let listaCachorros = buscarPets();
 
+/* Obter os parâmetros da query string */
+
+const idServico = obterValorParametroURL("idServico");
+const servicoEditado = buscarAtendimento(idServico);
+
 /* Atualiza a lista de atendimentos cadastrados com o conteúdo do Local Storage */
 
 let listaServicos = buscarAtendimentos();
 
 /* Carregar lista dos nomes dos cachorros cadastrados para seleção*/
+
 Object.values(listaCachorros).forEach((cachorro) => {
 	const item = document.createElement("option");
 	item.textContent = cachorro.nomeCachorro;
+	item.setAttribute("name", "dog");
 	listaNomeCachorrosEl.appendChild(item);
 });
+if (servicoEditado) {
+	const optionListEl = document.getElementsByName("dog");
+	for (option of optionListEl) {
+		if (option.textContent === servicoEditado.nomeCachorro)
+			option.setAttribute("selected", "true");
+	}
+}
 
 btnEl.addEventListener("click", salvarServico);
+
+/* Muda Dados da página conforme a query string recebida */
+if (servicoEditado) {
+	tituloPaginaEl.textContent = "Edita atendimento:";
+	horarioEl.value = servicoEditado.horarioAtendimento;
+	servicosSelecionados = servicoEditado.tipoServico;
+	responsavelEl.value = servicoEditado.funcionarioResponsavel;
+	verificaServicosSelecionados();
+	btnEl.textContent = "Salvar alterações";
+	btnEl.removeEventListener("click", salvarServico);
+	btnEl.addEventListener("click", atualizaServicos);
+}
 
 /* FUNÇÕES */
 
@@ -53,8 +79,13 @@ function calculaValorServicos(servicosSelecionados) {
 			total += 10.0;
 		}
 	}
-	totalEl.textContent = `Atendimento agendado com sucesso! - Total: R$ ${total},00`;
-	return total;
+	if (servicoEditado) {
+		totalEl.textContent = `Total: R$ ${total},00`;
+		return total;
+	} else {
+		totalEl.textContent = `Total: R$ ${total},00`;
+		return total;
+	}
 }
 
 function cadastrarNovoServico(servicosSelecionados, total) {
@@ -97,4 +128,40 @@ function salvarServico() {
 	const servicosSelecionados = capturaServicosSelecionados();
 	const total = calculaValorServicos(servicosSelecionados);
 	cadastrarNovoServico(servicosSelecionados, total);
+	alert("Atendimento agendado com sucesso!");
+}
+
+function verificaServicosSelecionados() {
+	for (servico of servicoEditado.tipoServico) {
+		servicosEl.forEach((nomeServico) => {
+			if (servico === nomeServico.value) {
+				nomeServico.checked = true;
+			}
+		});
+	}
+}
+
+function atualizaServicos() {
+	const servicosSelecionados = capturaServicosSelecionados();
+	const total = calculaValorServicos(servicosSelecionados);
+	atualizaNovoServico(servicosSelecionados, total);
+}
+
+function atualizaNovoServico(servicosSelecionados, total) {
+	const novoServico = {
+		nomeCachorro: listaNomeCachorrosEl.value,
+		horarioAtendimento: horarioEl.value,
+		tipoServico: servicosSelecionados,
+		funcionarioResponsavel: responsavelEl.value,
+		statusAtendimento: "Agendado",
+		valorServico: total,
+		fotoCachorro: buscarFotoPet(listaNomeCachorrosEl.value),
+		idCachorro: buscaIdPet(listaNomeCachorrosEl.value),
+		id: idServico,
+	};
+	atualizarAtendimento(novoServico);
+	setTimeout(() => {
+		alert("Atendimento alterado com sucesso!");
+		window.location.href = "./atendimentos.html";
+	}, "500");
 }
